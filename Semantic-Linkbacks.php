47915b8a677be0e46cf1def92491453a7f186328
pfefferle <pfefferle@gmail.com>
2013-12-18 13:49:06 +0100

    a lot of changes!

--- Semantic-Linkbacks.php
+++ Semantic-Linkbacks.php
@@ -2,220 +2,72 @@
 /*
  Plugin Name: Semantic-Linkbacks
  Plugin URI: https://github.com/acegiak/Semantic-Linkbacks
- Description: Semantic Linkbacks for semantic_linkbacks and pingbacks
+ Description: Semantic Linkbacks for webmentions, trackbacks and pingbacks
  Author: pfefferle & acegiak
  Author URI: http://notizblog.org/
  Version: 2.0.0-dev
 */
-require_once "Mf2/Parser.php";
-use Mf2\Parser;
+
+require_once "mf-linkbacks.php";
 
 /**
- * all supported url types
  *
- * @return array
  */
-function semantic_linkback_get_supported_url_types() {
-  return apply_filters("semantic_linkback_supported_url_types", array("in-reply-to", "like", "mention"));
-}
-
-/**
- * helper to find the correct h-entry node
- * 
- * @param array $mf_array the parsed microformats array
- * @param string $target the target url
- * @return array|false the h-entry node or false
- */
-function semantic_linkback_hentry_walker( $mf_array, $target ) {
-  // some basic checks
-  if ( !is_array( $mf_array ) )
-    return false;
-  if ( !isset( $mf_array["items"] ) )
-    return false;
-  if ( count( $mf_array["items"] ) == 0 )
-    return false;
-  
-  // iterate array
-  foreach ($mf_array["items"] as $mf) {    
-    if ( isset( $mf["type"] ) ) {
-      // only h-entries are important
-      if ( in_array( "h-entry", $mf["type"] ) ) {
-        if ( isset( $mf['properties'] ) ) {
-          // check properties if target urls was mentioned
-          foreach ($mf['properties'] as $key => $values) {
-            // check u-* params at first      
-            if ( in_array( $key, semantic_linkback_get_supported_url_types() )) {
-              foreach ($values as $value) {
-                if ($value == $target) {
-                  return $mf['properties'];
-                }
-              }
-            // check content as fallback
-            } elseif ( in_array( $key, array("content", "summary", "name")) && preg_match_all("|<a[^>]+?".preg_quote($target, "|")."[^>]*>([^>]+?)</a>|", $values[0][0], $context) ) {
-              return $mf['properties'];
-            }
-          }
-        }
-      // if root is h-feed, than hop into the "children" array to find some
-      // h-entries
-      } elseif ( in_array( "h-feed", $mf["type"]) && isset($mf['children']) ) {
-        $temp = array("items" => $mf['children']);
-        return semantic_linkback_hentry_walker($temp, $target);
-      }
-    }
-  }
-  
-  return false;
-}
-
-function semantic_linkback_to_comment( $html, $source, $target, $post, $commentdata = null ) {
-  global $wpdb;
-  
-  // check commentdata
-  if ( $commentdata == null ) {
-    $comment_post_ID = (int) $post->ID;
-    $commentdata = array('comment_post_ID' => $comment_post_ID, 'comment_author' => '', 'comment_author_url' => '', 'comment_author_email' => '', 'comment_content' => '', 'comment_type' => '', 'comment_ID' => '');
-    
-    if ( $comments = get_comments( array('meta_key' => 'semantic_linkback_source', 'meta_value' => $source) ) ) {
-      $comment = $comments[0];
-      $commentdata['comment_ID'] = $comment->comment_ID;
-    }
-  }
-  
-  // check if there is a parent comment
-  if ( $query = parse_url($target, PHP_URL_QUERY) ) {
-    parse_str($query);
-    if (isset($replytocom) && get_comment($replytocom)) {
-      $commentdata['comment_parent'] = $replytocom;
-    }
-  }
-  
-  // reset content type
-  $commentdata['comment_type'] = '';
-  
-  // parse source html
-  $parser = new Parser( $html );
-  $result = $parser->parse(true);
-  
-  // search for a matching h-entry
-  $hentry = semantic_linkback_hentry_walker($result, $target);
-  
-  if (!$hentry) {
-    header('HTTP/1.1 400 Bad Request');
-    echo json_encode(array("error"=> "no_link_found"));
-    exit;
-  }
-  
-  // try to find some content
-  // @link http://indiewebcamp.com/comments-presentation
-  if (isset($hentry['summary'])) {
-    $commentdata['comment_content'] = $wpdb->escape($hentry['summary'][0]);
-  } elseif (isset($hentry['content'])) {
-    $commentdata['comment_content'] = $wpdb->escape($hentry['content'][0]);
-  } elseif (isset($hentry['name'])) {
-    $commentdata['comment_content'] = $wpdb->escape($hentry['name'][0]);
-  } else {
-    header('HTTP/1.1 400 Bad Request');
-    echo json_encode(array("error"=> "no_content_found"));
-    exit;
-  }
-  
-  // set the right date
-  if (isset($hentry['published'])) {
-    $time = strtotime($hentry['published'][0]);
-    $commentdata['comment_date'] = date("Y-m-d H:i:s", $time);
-  } elseif (isset($hentry['updated'])) {
-    $time = strtotime($hentry['updated'][0]);
-    $commentdata['comment_date'] = date("Y-m-d H:i:s", $time);
+class SemanticLinkbacksPlugin {
+  /**
+   * constructor
+   */
+  public function __construct() {
+    add_action('init', array( $this, 'init' ));
   }
 
-  $author = null;
-  
-  // check if h-card has an author
-  if ( isset($hentry['author']) && isset($hentry['author'][0]['properties']) ) {
-    $author = $hentry['author'][0]['properties'];
+  /**
+   * Initialize the plugin, registering WordPess hooks.
+   */
+  public function init() {
+    // hook into linkback functions to add more semantics
+    add_action('pingback_post', array( $this, 'linkback_fix' ));
+    add_action('trackback_post', array( $this, 'linkback_fix' ));
+    add_action('webmention_post', array( $this, 'linkback_fix' ));
   }
-  
-  // else get representative hcard
-  if (!$author) {
-    foreach ($result["items"] as $mf) {    
-      if ( isset( $mf["type"] ) ) {
-        if ( in_array( "h-card", $mf["type"] ) ) {
-          // check domain
-          if (isset($mf['properties']) && isset($mf['properties']['url'])) {
-            foreach ($mf['properties']['url'] as $url) {
-              if (parse_url($url, PHP_URL_HOST) == parse_url($source, PHP_URL_HOST)) {
-                $author = $mf['properties'];
-                break;
-              }
-            }
-          }
-        }
-      }
+
+  /**
+   *
+   *
+   * @param int $comment_ID the comment id
+   */
+  public function linkback_fix($comment_ID) {
+    // check if it is a valid comment
+    $commentdata = get_comment($comment_ID, ARRAY_A);
+
+    if (!$commentdata) {
+      return false;
     }
-  }
-        
-  // if author is present use the informations for the comment
-  if ($author) {
-    if (isset($author['name'])) {
-      $commentdata['comment_author'] = $wpdb->escape($author['name'][0]);
+
+    // check if post exists
+    $post = get_post($commentdata['comment_post_ID'], ARRAY_A);
+
+    if (!$post) {
+      return false;
     }
-    
-    if (isset($author['email'])) {
-      $commentdata['comment_author_email'] = $wpdb->escape($author['email'][0]);
+
+    // get remote html
+    $target = get_permalink( $post['ID'] );
+    $response = wp_remote_get( $commentdata['comment_author_url'] );
+
+    if ( is_wp_error( $response ) ) {
+      return false;
     }
-    
-    if (isset($author['url'])) {
-      $commentdata['comment_author_url'] = $wpdb->escape($author['url'][0]);
+
+    $source_html = wp_remote_retrieve_body( $response );
+
+    //
+    $commentdata = apply_filters("semantic_linkbacks_commentdata", array(), $commentdata, $target, $source_html);
+
+    if (!empty($commentdata)) {
+      wp_update_comment($commentdata);
     }
-  }
-  
-  // check if it is a new comment or an update
-  if ( $commentdata['comment_ID'] ) {
-    wp_update_comment($commentdata);
-    $comment_ID = $commentdata['comment_ID'];
-  } else {
-    $comment_ID = wp_insert_comment($commentdata);
-  }
-  
-  // add source url as comment-meta
-  add_comment_meta( $comment_ID, "semantic_linkback_source", $source, true );
-  
-  if (isset($author['photo'])) {
-    // add photo url as comment-meta
-    add_comment_meta( $comment_ID, "semantic_linkback_avatar", $author['photo'][0], true );
   }
 }
 
-function linkback_fix($comment_ID) {
-  $commentdata = get_comment($comment_ID, ARRAY_A);
-  
-  if (!$commentdata) {
-    return false;
-  }
-  
-  $post = get_post($commentdata['comment_post_ID'], ARRAY_A);
-  
-  if (!$post) {
-    return false;
-  }
-  
-  $target = get_permalink($post['ID']);
-  $response = wp_remote_get( $commentdata['comment_author_url'] );
-  
-  if ( is_wp_error( $response ) ) {
-    return false;
-  }
-
-  $html = wp_remote_retrieve_body( $response );
-
-  semantic_linkback_to_comment( $html, $commentdata['comment_author_url'], $target, $post, $commentdata );
-}
-
-add_action( 'pingback_post', 'linkback_fix', 90, 1 );
-add_action( 'trackback_post', 'linkback_fix', 90, 1 ); 
-add_action( 'webmention_post', 'linkback_fix', 90, 1 );
-
-
-
-?>
+new SemanticLinkbacksPlugin;
---
+++ mf-linkbacks.php
@@ -0,0 +1,257 @@
+<?php
+require_once "Mf2/Parser.php";
+
+use Mf2\Parser;
+
+/**
+ *
+ */
+class MfLinkbacks {
+  // the parsed microformats objects
+  private $entries = null;
+
+  // the entry of interest
+  private $entry = null;
+
+  // the type of the linkback
+  private $type = "mention";
+
+  /**
+   * constructor
+   */
+  public function __construct() {
+    add_action('init', array( $this, 'init' ));
+  }
+
+  /**
+   * Initialize the plugin, registering WordPess hooks.
+   */
+  public function init() {
+    //
+    add_filter('semantic_linkbacks_commentdata', array( $this, 'generate_commentdata' ), 1, 4);
+  }
+
+  /**
+   * all supported url types
+   *
+   * @return array
+   */
+  public function get_class_mapper() {
+    $class_mapper = array();
+
+    /*
+     * replies
+     * @link http://indiewebcamp.com/replies
+     */
+    $class_mapper["in-reply-to"] = "reply";
+    $class_mapper["reply"]       = "reply";
+    $class_mapper["reply-of"]    = "reply";
+
+    /*
+     * likes
+     * @link http://indiewebcamp.com/likes
+     */
+    $class_mapper["like"]        = "like";
+    $class_mapper["like-of"]     = "like";
+
+    /*
+     * mentions
+     * @link http://indiewebcamp.com/mentions
+     */
+    $class_mapper["mention"]     = "mention";
+
+    return apply_filters("mf_linkbacks_class_mapper", $class_mapper);
+  }
+
+  /**
+   * all supported url types
+   *
+   * @return array
+   */
+  public function get_rel_mapper() {
+    $rel_mapper = array();
+
+    return apply_filters("mf_linkbacks_rel_mapper", $rel_mapper);
+  }
+
+  /**
+   *
+   */
+  public function generate_commentdata($commentdata, $original_commentdata, $target, $html) {
+    global $wpdb;
+
+    // parse source html
+    $parser = new Parser( $html );
+    $mf_array = $parser->parse(true);
+
+    // get all "relevant" entries
+    $entries = $this->get_entries($mf_array);
+
+    // check if there are any entries
+    if (empty($entries)) {
+      return array();
+    }
+
+    // get the entry of interest
+    $entry = $this->get_representative_entry($entries, $target);
+
+    // check if there is a representative entry
+    if (empty($entry)) {
+      return array();
+    }
+
+    $properties = $entry['properties'];
+
+    // try to find some content
+    // @link http://indiewebcamp.com/comments-presentation
+    if (isset($properties['summary'])) {
+      $commentdata['comment_content'] = esc_sql($properties['summary'][0]);
+    } elseif (isset($properties['content'])) {
+      $commentdata['comment_content'] = esc_sql($properties['content'][0]['html']);
+    } elseif (isset($properties['name'])) {
+      $commentdata['comment_content'] = esc_sql($properties['name'][0]);
+    }
+
+    // set the right date
+    if (isset($properties['published'])) {
+      $time = strtotime($properties['published'][0]);
+      $commentdata['comment_date'] = date("Y-m-d H:i:s", $time);
+    } elseif (isset($properties['updated'])) {
+      $time = strtotime($properties['updated'][0]);
+      $commentdata['comment_date'] = date("Y-m-d H:i:s", $time);
+    }
+
+    $author = null;
+
+    // check if h-card has an author
+    if ( isset($properties['author']) && isset($properties['author'][0]['properties']) ) {
+      $author = $properties['author'][0]['properties'];
+    }
+
+    // else get representative hcard
+    if (!$author) {
+      foreach ($mf_array["items"] as $mf) {
+        if ( isset( $mf["type"] ) ) {
+          if ( in_array( "h-card", $mf["type"] ) ) {
+            // check domain
+            if (isset($mf['properties']) && isset($mf['properties']['url'])) {
+              foreach ($mf['properties']['url'] as $url) {
+                if (parse_url($url, PHP_URL_HOST) == parse_url($source, PHP_URL_HOST)) {
+                  $author = $mf['properties'];
+                  break;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    // if author is present use the informations for the comment
+    if ($author) {
+      if (isset($author['name'])) {
+        $commentdata['comment_author'] = esc_sql($author['name'][0]);
+      }
+
+      if (isset($author['email'])) {
+        $commentdata['comment_author_email'] = esc_sql($author['email'][0]);
+      }
+
+      if (isset($author['url'])) {
+        $commentdata['comment_author_url'] = esc_sql($author['url'][0]);
+      }
+    }
+
+    $commentdata['comment_post_ID'] = $original_commentdata["comment_post_ID"];
+    $commentdata['comment_ID'] = $original_commentdata["comment_ID"];
+
+    //if ( $comments = get_comments( array('meta_key' => 'semantic_linkback_source', 'meta_value' => $source) ) ) {
+    //  $comment = $comments[0];
+    //  $commentdata['comment_ID'] = $comment->comment_ID;
+
+      //@todo delete pingback
+    //}
+
+    // check if there is a parent comment
+    if ( $query = parse_url($target, PHP_URL_QUERY) ) {
+      parse_str($query);
+      if (isset($replytocom) && get_comment($replytocom)) {
+        $commentdata['comment_parent'] = $replytocom;
+      }
+    }
+
+    return $commentdata;
+  }
+
+  public function get_entries($mf_array) {
+    $entries = array();
+
+    // some basic checks
+    if ( !is_array( $mf_array ) )
+      return $entries;
+    if ( !isset( $mf_array["items"] ) )
+      return $entries;
+    if ( count( $mf_array["items"] ) == 0 )
+      return $entries;
+
+    // get first item
+    $first_item = $mf_array["items"][0];
+
+    // check if it is an h-feed
+    if ( isset($first_item['type']) && in_array( "h-feed", $first_item["type"]) && isset($first_item['children']) ) {
+      $mf_array["items"] = $first_item['children'];
+    }
+
+    // iterate array
+    foreach ($mf_array["items"] as $mf) {
+      if ( isset( $mf["type"] ) && in_array( "h-entry", $mf["type"] ) ) {
+        $entries[] = $mf;
+      }
+    }
+
+    // return entries
+    return $entries;
+  }
+
+  /**
+   * helper to find the correct h-entry node
+   *
+   * @param array $mf_array the parsed microformats array
+   * @param string $target the target url
+   * @return array|false the h-entry node or false
+   */
+  public function get_representative_entry( $entries, $target ) {
+    // iterate array
+    foreach ($entries as $entry) {
+      // check properties
+      if ( isset( $entry['properties'] ) ) {
+        // check properties if target urls was mentioned
+        foreach ($entry['properties'] as $key => $values) {
+          $classes = $this->get_class_mapper();
+
+          // check u-* params
+          if ( in_array( $key, array_keys($classes) ) ) {
+            foreach ($values as $value) {
+              if ($value == $target) {
+                $this->type = $classes[$key];
+
+                return $entry;
+              }
+            }
+          }
+        }
+
+        // check properties if target urls was mentioned
+        foreach ($entry['properties'] as $key => $values) {
+          if ( $key == "content" && preg_match_all("|<a[^>]+?".preg_quote($target, "|")."[^>]*>([^>]+?)</a>|", $values[0]['html'], $context) ) {
+            return $entry;
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+}
+
+new MfLinkbacks();
